c3c31344e8b6ac3acb5e82747feb82eca4fd3d0a
UIOhumbert <thehumbert@hotmail.com>
2020-08-11 11:36:39 -0500

    first commit

---
+++ app/Http/Controllers/DatoController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Dato;
+
+class DatoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Dato::all();
+    }
+
+ 
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Dato::create($request->all());
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $dat = Dato::findOrFail($id);
+        $dat->numero = $request->numero;
+        $dat->update();
+        return dat();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $dat = Dato::findOrFail($id);
+        $dat->delete(); 
+    }
+}
